15210998859c5cd30a08b7e050c98a9817be18fd
Can202 <mgoopazo@gmail.com>
2021-09-16 19:20:19 -0300

    order

--- create-idur.py
+++ create-idur.py
@@ -1,240 +1,246 @@
 #!/usr/bin/python3
 import os
-Name=""
-Version=""
-it=True
-iti=True
-Depends=[]
-idurDepends=[]
-Conflict=[]
-i=0
 import sys
 from sys import exit
-if len(sys.argv) > 1:
-	if sys.argv[1] == "-v" or sys.argv[1] == "--version":
-		print("v0.2")
-		exit()
 
-print("Welcome! to create idur")
 
-while it:
-	Name = input("Name of your program: ")
-	if " " in Name:
-		print("Without space")
-	elif "\n" in Name:
-		print("no new lines")
-	elif Name == "":
-		print("write something")
-	else:
-		it=False
-it=True
+def main():
+	print("Welcome to Create Idur!")
+	if len(sys.argv) > 1:
+		if sys.argv[1] == "-v" or sys.argv[1] == "--version":
+			print("v0.2")
+			exit()
+	create_idur()
 
-while it:
-	Version = input("Version of your program: ")
-	if " " in Version:
-		print("Without space")
-	elif "\n" in Version:
-		print("no new lines")
-	elif Version == "":
-		print("write something")
-	else:
-		it=False
-it=True
-i=0
-ask=input("do your program need apt depends? (y/n)")
-if ask == "y" or ask == "Y":
-	while iti:
-		while it:
-			Depends.append(input("an apt depend: "))
-			if " " in Depends[i]:
-				print("Without space")
-			elif "\n" in Depends[i]:
-				print("no new lines")
-			elif Depends[i] == "":
-				print("write something")
-			else:
-				it=False
-		it=True
-		ask=input("more? (y/n)")
-		if ask == "y" or ask == "Y":
-			iti=True
-		else:
-			iti=False
-		i=i+1
+def create_idur():
+	Name=""
+	Version=""
+	it=True
 	iti=True
-it=True
+	Depends=[]
+	idurDepends=[]
+	Conflict=[]
+	i=0
 
-i=0
-ask=input("do your program need idur depends? (y/n)")
-if ask == "y" or ask == "Y":
-	while iti:
-		while it:
-			idurDepends.append(input("an idur depend: "))
-			if " " in idurDepends[i]:
-				print("Without space")
-			elif "\n" in idurDepends[i]:
-				print("no new lines")
-			elif idurDepends[i] == "":
-				print("write something")
-			else:
-				it=False
-		it=True
-		ask=input("more? (y/n)")
-		if ask == "y" or ask == "Y":
-			iti=True
+	while it:
+		Name = input("Name of your program: ")
+		if " " in Name:
+			print("Without space")
+		elif "\n" in Name:
+			print("no new lines")
+		elif Name == "":
+			print("write something")
 		else:
-			iti=False
-		i=i+1
-	iti=True
-it=True
-i=0
-ask=input("conflict with other packages? (y/n)")
-if ask == "y" or ask == "Y":
-	while iti:
-		while it:
-			Conflict.append(input("a conflict: "))
-			if " " in Conflict[i]:
-				print("Without space")
-			elif "\n" in Conflict[i]:
-				print("no new lines")
-			elif Conflict[i] == "":
-				print("write something")
-			else:
-				it=False
-		it=True
-		ask=input("more? (y/n)")
-		if ask == "y" or ask == "Y":
-			iti=True
+			it=False
+	it=True
+
+	while it:
+		Version = input("Version of your program: ")
+		if " " in Version:
+			print("Without space")
+		elif "\n" in Version:
+			print("no new lines")
+		elif Version == "":
+			print("write something")
 		else:
-			iti=False
-		i=i+1
-	iti=True
-it=True
+			it=False
+	it=True
+	i=0
+	ask=input("do your program need apt depends? (y/n)")
+	if ask == "y" or ask == "Y":
+		while iti:
+			while it:
+				Depends.append(input("an apt depend: "))
+				if " " in Depends[i]:
+					print("Without space")
+				elif "\n" in Depends[i]:
+					print("no new lines")
+				elif Depends[i] == "":
+					print("write something")
+				else:
+					it=False
+			it=True
+			ask=input("more? (y/n)")
+			if ask == "y" or ask == "Y":
+				iti=True
+			else:
+				iti=False
+			i=i+1
+		iti=True
+	it=True
 
-while it:
-	ask=input("""architecture:
+	i=0
+	ask=input("do your program need idur depends? (y/n)")
+	if ask == "y" or ask == "Y":
+		while iti:
+			while it:
+				idurDepends.append(input("an idur depend: "))
+				if " " in idurDepends[i]:
+					print("Without space")
+				elif "\n" in idurDepends[i]:
+					print("no new lines")
+				elif idurDepends[i] == "":
+					print("write something")
+				else:
+					it=False
+			it=True
+			ask=input("more? (y/n)")
+			if ask == "y" or ask == "Y":
+				iti=True
+			else:
+				iti=False
+			i=i+1
+		iti=True
+	it=True
+	i=0
+	ask=input("conflict with other packages? (y/n)")
+	if ask == "y" or ask == "Y":
+		while iti:
+			while it:
+				Conflict.append(input("a conflict: "))
+				if " " in Conflict[i]:
+					print("Without space")
+				elif "\n" in Conflict[i]:
+					print("no new lines")
+				elif Conflict[i] == "":
+					print("write something")
+				else:
+					it=False
+			it=True
+			ask=input("more? (y/n)")
+			if ask == "y" or ask == "Y":
+				iti=True
+			else:
+				iti=False
+			i=i+1
+		iti=True
+	it=True
 
-	all: one method to install with 64bits and 32bits
-	x86_64: just 64bits
-	i386: just 32bits
-	both: one method to 64bits and other to 32bits
+	while it:
+		ask=input("""architecture:
 
-What is the architecture? 
+		all: one method to install with 64bits and 32bits
+		x86_64: just 64bits
+		i386: just 32bits
+		both: one method to 64bits and other to 32bits
 
-	""")
-	if ask == "all" :
-		ARCH="all"
-		it=False
-	elif ask == "x86_64" :
-		ARCH="x86_64"
-		it=False
-	elif ask == "i386" :
-		ARCH="i386"
-		it=False
-	elif ask == "both" :
-		ARCH="both"
-		it=False
+	What is the architecture? 
+
+		""")
+		if ask == "all" :
+			ARCH="all"
+			it=False
+		elif ask == "x86_64" :
+			ARCH="x86_64"
+			it=False
+		elif ask == "i386" :
+			ARCH="i386"
+			it=False
+		elif ask == "both" :
+			ARCH="both"
+			it=False
+		else:
+			print("error")
+	it=True
+		
+	YourName=input("Your Name: ")
+	YourEmail=input("Your Email: ")
+
+	FILE="""
+	Name=\"""" + Name + """\"
+	Version=\"""" + Version + """\"
+
+
+	Maintainer=\"""" + YourName + """\"
+	Contact=\"""" + YourEmail + """\"
+
+	Arch=\"""" + ARCH + """\"
+
+	"""
+	if len(Depends) != 0:
+		FILE += "Depends= ["
+		for ii in range(len(Depends)):
+			FILE += "\""
+			FILE += Depends[ii]
+			FILE += "\""
+			if ii != len(Depends) -1:
+				FILE += ", "
+		FILE += "]\n"
+		
+	if len(idurDepends) != 0:
+		FILE += "idurDepends= ["
+		for ii in range(len(idurDepends)):
+			FILE += "\""
+			FILE += idurDepends[ii]
+			FILE += "\""
+			if ii != len(idurDepends) -1:
+				FILE += ", "
+		FILE += "]\n"
+		
+	if len(Conflict) != 0:
+		FILE += "Conflict= [\"" + Name + "\", "
+		for ii in range(len(Conflict)):
+			FILE += "\""
+			FILE += Conflict[ii]
+			FILE += "\""
+			if ii != len(Conflict) -1:
+				FILE += ", "
+		FILE += "]\n"
 	else:
-		print("error")
-it=True
-	
-YourName=input("Your Name: ")
-YourEmail=input("Your Email: ")
+		FILE += "Conflict= [\"" + Name + "\"]"
+		
 
-FILE="""
-Name=\"""" + Name + """\"
-Version=\"""" + Version + """\"
-
-
-Maintainer=\"""" + YourName + """\"
-Contact=\"""" + YourEmail + """\"
-
-Arch=\"""" + ARCH + """\"
-
-"""
-if len(Depends) != 0:
-	FILE += "Depends= ["
-	for ii in range(len(Depends)):
-		FILE += "\""
-		FILE += Depends[ii]
-		FILE += "\""
-		if ii != len(Depends) -1:
-			FILE += ", "
-	FILE += "]\n"
-	
-if len(idurDepends) != 0:
-	FILE += "idurDepends= ["
-	for ii in range(len(idurDepends)):
-		FILE += "\""
-		FILE += idurDepends[ii]
-		FILE += "\""
-		if ii != len(idurDepends) -1:
-			FILE += ", "
-	FILE += "]\n"
-	
-if len(Conflict) != 0:
-	FILE += "Conflict= [\"" + Name + "\", "
-	for ii in range(len(Conflict)):
-		FILE += "\""
-		FILE += Conflict[ii]
-		FILE += "\""
-		if ii != len(Conflict) -1:
-			FILE += ", "
-	FILE += "]\n"
-else:
-	FILE += "Conflict= [\"" + Name + "\"]"
-	
-
-FILE += """
-Description=\"\"\"
-
-description here
-
-\"\"\"
-"""
-
-if ARCH == "all":
 	FILE += """
-Install=\"\"\"
+	Description=\"\"\"
 
-# Install instructions here (bash)
+	description here
 
-\"\"\"
-"""
-if ARCH == "x86_64" or ARCH == "both":
+	\"\"\"
+	"""
+
+	if ARCH == "all":
+		FILE += """
+	Install=\"\"\"
+
+	# Install instructions here (bash)
+
+	\"\"\"
+	"""
+	if ARCH == "x86_64" or ARCH == "both":
+		FILE += """
+	Install64=\"\"\"
+
+	# Install instructions here (bash)
+
+	\"\"\"
+	"""
+	if ARCH == "i386" or ARCH == "both":
+		FILE += """
+	Install32=\"\"\"
+
+	# Install instructions here (bash)
+
+	\"\"\"
+	"""
+		
+
+
 	FILE += """
-Install64=\"\"\"
+	Remove=\"\"\"
 
-# Install instructions here (bash)
+	# Remove instructions here (bash)
 
-\"\"\"
-"""
-if ARCH == "i386" or ARCH == "both":
-	FILE += """
-Install32=\"\"\"
+	\"\"\"
+	"""
+	os.system("clear")
+	print(FILE)
+	ask = input("Good? (Y/n)")
+	if ask == "Y" or ask == "y":
+		print("Saved")
+		TEMPFILE = open(Name + ".py", 'w')
+		TEMPFILE.write(str(FILE))
+	else:
+		print("Not saved")
 
-# Install instructions here (bash)
-
-\"\"\"
-"""
-	
-
-
-FILE += """
-Remove=\"\"\"
-
-# Remove instructions here (bash)
-
-\"\"\"
-"""
-os.system("clear")
-print(FILE)
-ask = input("Good? (Y/n)")
-if ask == "Y" or ask == "y":
-	print("Saved")
-	TEMPFILE = open(Name + ".py", 'w')
-	TEMPFILE.write(str(FILE))
-else:
-	print("Not saved")
-
-
+if __name__ == "__main__":
+	main()
